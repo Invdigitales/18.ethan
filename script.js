a33e82b751bd4bed23ec8e44ecbf52cf42ee5d44
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2026-05-05 12:29:40 -0300

    cuatro

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function diaEventoAhora () {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!3m2!1ses!2sar!4v1774965528920!5m2!1ses!2sar!6m8!1m7!1sC3HPgjQZT1qjVmcqokkuVw!2m2!1d-33.15530880692818!2d-62.85555713253889!3f252.11990488527778!4f6.559659852124497!5f0.7820865974627469" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.6843695817206!2d-58.677917918426786!3d-34.63741568949379!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcbf146a7b4575%3A0xc998de9bf68035a6!2sOribe%201290%2C%20B1714NJZ%20Ituzaing%C3%B3%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1777994893437!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
         
         
     })
